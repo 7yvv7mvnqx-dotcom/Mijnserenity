6a322b37fc93927df22c2fd854c60a0e5cb144aa
7yvv7mvnqx-dotcom <7yvv7mvnqx@privaterelay.appleid.com>
2026-08-14 21:26:27 +0200

    Load new Pro cockpit dashboard assets

--- dashboard-pro-71531-loader.js
+++ dashboard-pro-71531-loader.js
@@ -5,12 +5,12 @@
   const link=document.createElement('link');
   link.id='msPro71531Css';
   link.rel='stylesheet';
-  link.href='/dashboard-pro-71531.css?v=715311';
+  link.href='/dashboard-pro-71531.css?v=715320';
   document.head.appendChild(link);
   if(!document.querySelector('script[data-ms-pro-dashboard]')){
     const script=document.createElement('script');
     script.dataset.msProDashboard='1';
-    script.src='/dashboard-pro-71531.js?v=715311';
+    script.src='/dashboard-pro-71531.js?v=715320';
     script.async=false;
     document.head.appendChild(script);
   }
