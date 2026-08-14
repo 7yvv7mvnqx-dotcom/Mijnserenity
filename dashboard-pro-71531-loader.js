b71f6bdc4340171e3f7f4c195faa79d4c4091a60
7yvv7mvnqx-dotcom <7yvv7mvnqx@privaterelay.appleid.com>
2026-08-14 21:44:39 +0200

    Bump cockpit dashboard assets

--- dashboard-pro-71531-loader.js
+++ dashboard-pro-71531-loader.js
@@ -5,12 +5,12 @@
   const link=document.createElement('link');
   link.id='msPro71531Css';
   link.rel='stylesheet';
-  link.href='/dashboard-pro-71531.css?v=715320';
+  link.href='/dashboard-pro-71531.css?v=715330';
   document.head.appendChild(link);
   if(!document.querySelector('script[data-ms-pro-dashboard]')){
     const script=document.createElement('script');
     script.dataset.msProDashboard='1';
-    script.src='/dashboard-pro-71531.js?v=715320';
+    script.src='/dashboard-pro-71531.js?v=715330';
     script.async=false;
     document.head.appendChild(script);
   }
